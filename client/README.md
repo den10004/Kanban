8a006486fa5efe9b1270b0dc921f40189dcbb2c0
Denis Sergeev <denis-sergeev-85@inbox.ru>
2023-09-12 16:12:33 +0300

    начат клиент

---
+++ client/README.md
@@ -0,0 +1,3 @@
+administrator
+
+12345678
---
+++ client/src/assets/index.js
@@ -0,0 +1,16 @@
+import dark from "./images/logo-dark.png";
+import icon from "./images/favicon.png";
+
+const assets = {
+  images: {
+    // eslint-disable-next-line no-undef
+    logoDark: dark,
+    // eslint-disable-next-line no-undef
+    appIcon: icon,
+  },
+  colors: {
+    secondary: "#292929",
+  },
+};
+
+export default assets;
